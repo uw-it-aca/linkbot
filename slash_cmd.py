b622b35c73274f443e62497eb25bc6e3fe772fa7
mike seibel <mikes@washington.edu>
2021-05-18 19:16:33 -0700

    iron out slash commands

--- slash_cmd.py
+++ slash_cmd.py
@@ -6,35 +6,52 @@ def linkbot_command(ack, say, command, logger):
 
     ack()
     parts = command.get('text', '').split()
-    op = parts[0].lower()
-    argv = parts[1:]
-    if len(argv) == 0 or op in ['help', '?']:
+    op = parts[0].lower() if len(parts) > 0 else ''
+    argv = parts[1:] if len(parts) > 1 else [None]
+    if op in ['help', '?', '']:
         say("linkbot can:\n{}".format('   \n'.join([
             "debug [on|off]",
             "quips [on|off|reset]",
-            "links"])))
+            "links"])), parse='none')
     elif op == 'debug':
-        sense = argv[0].lower() in ['on', '1', 'true']
-        logger.setLevel(logging.DEBUG if sense else logging.INFO)
-        say("linkbot debug logging {}".format('on' if sense else 'off'))
-    elif op == 'quips':
-
-        arg = argv[0].lower()
-        if arg == 'reset':
-            for bot in bot_list:
-                bot.quip_reset()
-
-            say("linkbot quips have been reset")
+        if argv[0]:
+            sense = argv[0].lower() in ['on', '1', 'true']
+            logger.setLevel(logging.DEBUG if sense else logging.INFO)
+            say("linkbot debug logging {}".format('on' if sense else 'off'),
+                parse='none')
         else:
-            sense = arg in ['on', '1', 'true']
-            for bot in bot_list:
-                bot.quip = sense
+            say("linkbot debug is {}".format(logger.level == logging.DEBUG))
+    elif op == 'quips':
+        if argv[0]:
+            arg = argv[0].lower()
+            if arg == 'reset':
+                for bot in bot_list:
+                    bot.quip_reset()
 
-            say("linkbot turned {} quips".format('on' if sense else 'off'))
+                say("linkbot quips have been reset")
+            elif arg in ['on', 'off', 'true', 'false', '0', '1']:
+                sense = arg in ['on', '1', 'true']
+                for bot in bot_list:
+                    bot.quip = sense
+
+                say("linkbot turned {} quips".format('on' if sense else 'off'))
+            else:
+                say("unrecognized quips option")
+        else:
+            q = set()
+            for bot in bot_list:
+                for bq in bot.QUIPS:
+                    q.add(bq)
+
+            say("Current set of quips:\n".format("    \n".join(q)),
+                parse='none')
     elif op == 'links':
-        say("linkbot searches for:\n".format("    \n".join(
-            ["{}: {}".format(bot.name(), bot.match_pattern())
-             for bot in bot_list])))
+        if argv[0] is None:
+            say("linkbot searches for:\n".format("    \n".join(
+                ["{}: {}".format(bot.name(), bot.match_pattern())
+                 for bot in bot_list])), parse='none')
+        else:
+            say("unrecognized links option")
     else:
         say("sorry, linkbot cannot *{}*".format(op))
 
