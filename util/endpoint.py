34a71c7b6e4c514531b0261387e64ae877d7953a
Jim Laney <jlaney@uw.edu>
2022-05-24 09:23:18 -0700

    add pycodestyle to workflow and fix issues

--- util/endpoint.py
+++ util/endpoint.py
@@ -1,4 +1,4 @@
-# Copyright 2021 UW-IT, University of Washington
+# Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 """
 Functions providing linkbot events endpoint
@@ -9,6 +9,7 @@ from tornado.web import Application
 from tornado.ioloop import IOLoop
 
 tornado_api = None
+
 
 # prepare slack event endpoint
 def init_endpoint_server(slack_app):
